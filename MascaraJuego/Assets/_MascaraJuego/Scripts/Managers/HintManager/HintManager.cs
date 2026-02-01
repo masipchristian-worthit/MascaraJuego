e53f9de5a8f931f53a162b3dedcc339e73adb361
Underfide0 <ulisesgedo@gmail.com>
2026-02-01 06:14:05 +0100

    waos diablo

--- MascaraJuego/Assets/_MascaraJuego/Scripts/Managers/HintManager/HintManager.cs
+++ MascaraJuego/Assets/_MascaraJuego/Scripts/Managers/HintManager/HintManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class HintManager : MonoBehaviour
 {
@@ -33,30 +34,77 @@ public class HintManager : MonoBehaviour
     }
 
     public void ShowHintsInMenu()
+{
+    var _hintParentCanvas = _hintsParent.GetComponent<CanvasGroup>().alpha = 1;
+
+    // Eliminar los hints previos
+    for (int i = 0; i < _hintsParent.childCount; i++)
     {
-        var _hintParentCanvas = _hintsParent.GetComponent<CanvasGroup>().alpha = 1;
-        for (int i = 0; i < _hintsParent.childCount; i++)
-        {
-            Destroy(_hintsParent.GetChild(i).gameObject);
-            Debug.Log("Eliminando hint");
-        }
-
-        for (int i = 0; i < hints.Count; i++)
-        {
-            HintButton hintButtonPrefab = Instantiate(_hintButton, _hintsParent);
-            hintButtonPrefab.GetComponent<HintButton>().SetTextButton(hints[i]);
-            Debug.Log("Añadiendo hint ");
-        }
-
-        if (_hintsParent.GetChild(0).transform.GetChild(0).gameObject != null)
-        {
-            EventSystem.current.SetSelectedGameObject(_hintsParent.GetChild(0).transform.GetChild(0).gameObject);
-        }
+        Destroy(_hintsParent.GetChild(i).gameObject);
+        Debug.Log("Eliminando hint");
     }
+
+    // Crear los nuevos hint buttons
+    List<HintButton> hintButtons = new List<HintButton>(); // Para almacenar referencias a los botones instanciados
+    for (int i = 0; i < hints.Count; i++)
+    {
+        HintButton hintButtonPrefab = Instantiate(_hintButton, _hintsParent);
+        hintButtonPrefab.GetComponent<HintButton>().SetTextButton(hints[i]);
+        hintButtons.Add(hintButtonPrefab);  // Guardamos las referencias
+        Debug.Log("Añadiendo hint ");
+    }
+
+    // Configurar la navegación entre los botones
+    for (int i = 0; i < hintButtons.Count; i++)
+    {
+        Button button = hintButtons[i].GetComponent<Button>();
+        Navigation navigation = button.navigation;  // Crea una referencia a la variable Navigation
+
+        // Configurar la navegación hacia abajo
+        if (i < hintButtons.Count - 1)
+        {
+            // Si no es el último botón, navegamos hacia abajo al siguiente botón
+            navigation.selectOnUp = hintButtons[i + 1].GetComponent<Button>();  // El siguiente botón
+            navigation.selectOnDown = hintButtons[i + 1].GetComponent<Button>();  // El siguiente botón
+        }
+        else
+        {
+            // Si es el último botón, no hay un botón hacia abajo (se queda en el último)
+            navigation.selectOnUp = hintButtons[i - 1].GetComponent<Button>(); // El anterior
+            navigation.selectOnDown = button;  // Se queda en el mismo
+        }
+
+        // Configuración hacia arriba (para todos los botones)
+        if (i > 0)
+        {
+            navigation.selectOnUp = hintButtons[i - 1].GetComponent<Button>();  // El anterior
+        }
+
+        // Si es el primer botón, no navega hacia arriba, se queda en el mismo
+        if (i == 0)
+        {
+            navigation.selectOnUp = button;
+        }
+
+        // Asignamos la configuración de navegación de vuelta al botón
+        button.navigation = navigation;
+    }
+
+    // Seleccionar el primer botón para comenzar la navegación
+    if (_hintsParent.childCount > 0)
+    {
+        EventSystem.current.SetSelectedGameObject(_hintsParent.GetChild(0).gameObject);
+        Debug.Log("SELECCIONANDO EL PRIMER HINT");
+    }
+}
+
+
+
 
     public void HideHintMenu()
     {
-        var _hintParentCanvas = _hintsParent.GetComponent<CanvasGroup>().alpha = 0;
+        _hintMenu.CloseHint();
+        _hintsParent.GetComponent<CanvasGroup>().alpha = 0;
     }
 
     public void ShowHintInMenu(HintSO hint)
